bfffeb3100e034a4a86945b08e78f6c2ba58b868
Wellington Rodrigues <wellington.rodrigues@psychemedics.com.br>
2017-11-18 11:54:07 -0200

    Mongo Exemple

--- curso/app/MongoExemple.php
+++ curso/app/MongoExemple.php
@@ -6,5 +6,8 @@ use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
 class MongoExemple extends Eloquent
 {
-    //
+    protected $collection = 'mongo';//Nome do banco
+    protected $connection = 'mongodb';
+    protected $guarded = [];
+    protected $dateFormat = 'Y-m-d\TH:i:sP';
 }
